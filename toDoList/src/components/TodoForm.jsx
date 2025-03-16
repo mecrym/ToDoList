6928ff9d1885af5c49109b24d266c56badd47178
Maria Moreira <maria.ecr.moreira1@gmail.com>
2025-03-15 22:56:35 -0300

    fix: adding todos fixed

--- toDoList/src/components/TodoForm.jsx
+++ toDoList/src/components/TodoForm.jsx
@@ -1,22 +1,23 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
-const TodoForm = ({addTodo}) => {//na data está seguindo o padrão brasileiro, tem q mudar depois
-    const [description, setDescription]=useState("")
+const TodoForm = ({ addTodo }) => {
+    const [description, setDescription] = useState("")
     const [end_date, setEnd_date] = useState("")
 
-    const handleSubmit = (event) =>{
+    const handleSubmit = (event) => {
         event.preventDefault()
-        if(!description||!end_date) return
+        if (!description || !end_date) return
         addTodo(description, end_date)
         setDescription("")
         setEnd_date("")
         console.log(description, end_date)
     }
+
     return <div className="todo-form">
         <h2>New Task</h2>
-        <form onSubmit={handleSubmit}>
-            <input type="description" plac1eholder="What's your task?" value={description} onChange={(event)=> setDescription(event.target.description)}/>
-            <input type="date" value={end_date} onChange={(event)=> setEnd_date(event.target.end_date)}/>
+        <form onSubmit={handleSubmit}> 
+            <input type="text" placeholder="What's your task?" value={description} onChange={(event) => setDescription(event.target.value)} />
+            <input type="date" value={end_date} onChange={(event) => setEnd_date(event.target.value)} />
             <button type="submit">Create Task</button>
         </form>
     </div>
