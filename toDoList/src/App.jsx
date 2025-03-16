31735842a6f7a99a562efd4610625df549ea3e62
Maria Moreira <maria.ecr.moreira1@gmail.com>
2025-03-16 02:49:38 -0300

    feat: adding tabs for better visualization

--- toDoList/src/App.jsx
+++ toDoList/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css"
 import TodoForm from "./components/TodoForm";
 import Search from "./components/Search";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Tab, Tabs} from "react-bootstrap";
 
 function App() {
 	//aq está definindp os valores iniciais e onde as tarefas serão armazenadas
@@ -31,7 +32,9 @@ function App() {
 		}
 	])
 
-const [search, setSearch]=useState("")
+	const [search, setSearch]=useState("")
+	const [currentTab, setCurrentTab] = useState("active")
+
 	const addTodo = (description, end_date) => {
 		const newTodos = [...todos, {
 			id: Math.floor(Math.random()*1000),
@@ -42,7 +45,7 @@ const [search, setSearch]=useState("")
 		}]
 
 		setTodos(newTodos)
-	} 
+	}
 
 	const removeTodo = (id) => {
 		const newTodos = [...todos]
@@ -52,30 +55,51 @@ const [search, setSearch]=useState("")
 		setTodos(filteredTodos)
 	}
 
-	/**criar função para mover cards para outra aba */
-	const completeTodo = (id) => {
-		const newTodos = [...todos]
-		newTodos.map((todo) => todo.id == id ? todo.completed = !todo.completed : todo)
-		setTodos(newTodos)
-	}
 
-	const archivedTodo = (id) => {
-		const newTodos = [...todos]
-		newTodos.map((todo) => todo.id == id ? todo.archived = !todo.archived : todo)
-		setTodos(newTodos)
+	const completeTodo = (id) => {
+		setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo
+		))
 	}
+	/**criar função para mover cards para outra aba */
+	const archivedTodo = (id) => {
+		setTodos(todos.map(todo=>todo.id ===id? {...todo, archived: !todo.archived}: todo))
+	}
+	
+	const filteredTodos=todos.filter(todo=>{
+		const matchesSearsh = todo.description.toLowerCase().includes(search.toLowerCase())
+		switch (currentTab) {
+			case "active":
+				return matchesSearsh && !todo.completed && !todo.archived
+			case "completed":
+				return matchesSearsh && todo.completed && !todo.archived
+			case "archived":
+				return matchesSearsh && todo.archived
+			default:
+				return matchesSearsh
+		}
+	})
 
     return (
 		<div className="app">
 			<h1>Your ToDo List!</h1>
+
+
+
 			<Search search={search} setSearch={setSearch}/>
+
+		{/**lembre de alterar isso se necessario, aq q da bo nas tabs */}
+			<Tabs accessKey={currentTab} onSelect={(key) => setCurrentTab(key)} className="mb-3">
+				<Tab eventKey="active" title={`Active (${todos.filter(task => !task.completed && !task.archived).length})`}></Tab>
+				<Tab eventKey="completed" title={`Completed (${todos.filter(task => task.completed && !task.archived).length})`}></Tab>
+				<Tab eventKey="archived" title={`Archived (${todos.filter(task => task.archived).length})`}></Tab>
+			</Tabs>
+
 			<div className="todo-list">
-				{todos.filter((todo)=>todo.description.toLowerCase().includes(search.toLowerCase()))
-				.map((todo) => (//passar pro cadesno do caos, func array q percorre todos os itens do todo
+				{filteredTodos.map((todo) => (//passar pro cadesno do caos, func array q percorre todos os itens do todo
 					<Todo key={todo.id} todo = {todo} removeTodo={removeTodo} completeTodo={completeTodo} archivedTodo={archivedTodo}/>
 				))}
 			</div>
-			<TodoForm addTodo={addTodo}/>
+			{currentTab === "active" && <TodoForm addTodo={addTodo}/>}
 		</div>
 	);
 }
